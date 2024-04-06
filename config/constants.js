555016bf86feb25cccbd260ae5c6582d7f6f057d
waitsaber <lyq_dengdai@163.com>
2024-04-07 00:09:00 +0800

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -10,7 +10,7 @@ let config = {
     Cursor: false, // 使用自定义光标图片
   
     LanguageCategories: ["日语", "英语"], // 语言分类
-    RemarkCategories: ["原创", "合唱","舰长"], // 标签分类
+    RemarkCategories: ["原创", "合唱","付费"], // 标签分类
   
     BannerTitle: "蝶礼_Deli", // banner 标题
   
